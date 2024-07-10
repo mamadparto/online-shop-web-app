30e241a602b54dd2aead47bde97308cb15f9633f
mohammad partonia <partonia.m@gmail.com>
2024-07-10 18:06:35 +0330

    create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+status = (
+    ('available', 'available'),
+    ('unavailable', 'unavailable'),
+    ('coming_soon', 'comming soon')
+)
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    discription = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length=12 , choices=status)
+
+
+    datetime_created = models.DateTimeField(auto_created=True)
+    datetime_modified = models.DateTimeField(auto_created=True)
