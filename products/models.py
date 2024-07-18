7830eb175eb33e4f378208f2d8fb921cf89deefd
mohammad partonia <partonia.m@gmail.com>
2024-07-18 21:00:28 +0330

    add static files

--- products/models.py
+++ products/models.py
@@ -20,6 +20,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
-        return reverse('product_detail', args=[self.pk])
+        return reverse('product_detail', args=[self.id])
