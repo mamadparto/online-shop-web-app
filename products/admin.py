107e0ca48d2ad60c7f516c4eed09710afe96fc33
mohammad partonia <partonia.m@gmail.com>
2024-07-19 10:56:42 +0330

    add comment models

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ProductComment
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'status','active']
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'product', 'stars', 'active']
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductComment, )
